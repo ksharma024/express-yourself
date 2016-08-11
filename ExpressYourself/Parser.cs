277322f5cd504f11e25c88a546bfb07297f10561
Karn Sharma <karn.sharma024@gmail.com>
2016-08-10 21:09:57 -0400

    Done?

--- ExpressYourself/Parser.cs
+++ ExpressYourself/Parser.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ExpressYourself
@@ -15,26 +16,58 @@ namespace ExpressYourself
         /// <returns>the title string if it exists</returns>
         public static string GetTitle(string str)
         {
-            // TODO
-            return "";
+            var titleExpression = new Regex(@"title\: (.*),+");
+            var match = titleExpression.Match(str);
+            if (!match.Success)
+            {
+                return "NOT FOUND";
+            }
+            else
+            {
+                return match.Groups[1].Value;
+            }
         }
 
         public static string GetType(string str)
         {
-            // TODO
-            return "";
+            var typeExpression = new Regex(@"(\b[A-Z]*[a-z]*),Title+\: .*,Length+\: [0-9]*[a-z]* [0-9]*[a-z]*");
+            var match = typeExpression.Match(str);
+            if (!match.Success)
+            {
+                return "LOL";
+            }
+            else
+            {
+                return match.Groups[1].Value;
+            }
         }
 
         public static string GetLength(string str)
         {
-            // TODO
-            return "";
+            var numberExpression = new Regex(@"\b[A-Z]*[a-z]*,Title+\: .*,Length+\: ([0-9]*[a-z]* [0-9]*[a-z]*)");
+            var match = numberExpression.Match(str);
+            if (!match.Success)
+            {
+                return "LOL";
+            }
+            else
+            {
+                return match.Groups[1].Value;
+            }
         }
 
         public static bool IsValidLine(string str)
         {
-            // TODO
-            return false;
+            var lineValue = new Regex(@"\b[A-Z]*[a-z]*,Title+\: .*,Length+\: [0-9]*[a-z]* [0-9]*[a-z]*");
+            var match = lineValue.Match(str);
+            if (!match.Success)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
     }
 }
